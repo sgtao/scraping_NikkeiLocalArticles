1b2442e7914120d480a0dad262e934b916f5e97e
Shogo Ogami <sg.tao.so@gmail.com>
2022-08-18 21:40:26 +0900

    add 21_split_articles for register article in each year folder

---
+++ 02_scraping_whole_articles/21_split_article_each_year.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+# 21_split_article_each_year.sh
+SCRIPT_NAME=` basename $0 `;
+usage() {
+  echo "
+$SCRIPT_NAME <+|-> <year> <csvfile> 
+  +|- : ommit flag. when + , extract year. when - , omit year
+  year : [2019,2020,2021]
+  csvfile : article file of economy, bussiness and technology
+"
+}
+#
+# varidate argments
+omitflag=$1;
+year=$2;
+csvfile=$3;
+if [ $# -lt 3 ]; then
+  usage; exit 1;
+fi
+if [ ! -f $csvfile ]; then
+  usage; exit 1;
+fi
+#
+grep_str="$year年..月..日,"
+echo "$grep_str" match articles : ;
+#
+grep -c "$grep_str" $csvfile
+if [ $? -ne 0 ] ; then
+    exit 1;
+fi
+#
+# prepare extract articles
+csvfilename=` basename $csvfile `;
+outputpath="$year/$csvfilename"
+if [ ! -d $year ] ; then
+    mkdir $year;
+fi
+head -1 $csvfile > $outputpath;
+if [ "$omitflag" = "+" ]; then
+    grep "$grep_str" $csvfile >> $outputpath;
+    echo "# output matched articles to $outputpath";
+else
+    grep -v "$grep_str" $csvfile > $outputpath;
+    echo "# output un-matched articles to $outputpath";
+fi
+exit 0;
+#
+# EOF
